5264dc4bb40a86172099e80b9254757db8672991
TAY.W.H <77138260+yinyangjia@users.noreply.github.com>
2026-02-20 23:11:33 +0800

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,1 +1,26 @@
-
+{
+    "gameInfo": {
+        "title": "Immortal Idle RPG",
+        "version": "1.0.0",
+        "saveKey": "ImmortalIdle_Save_V1"
+    },
+    "initialPlayerState": {
+        "rank": "凡人",
+        "rankIndex": 0,
+        "exp": 0,
+        "nextLevelExp": 100,
+        "gold": 0,
+        "inventory": [],
+        "lastUpdate": 0
+    },
+    "ranks": [
+        {"name": "凡人", "requirement": 0},
+        {"name": "炼气", "requirement": 100},
+        {"name": "筑基", "requirement": 500},
+        {"name": "金丹", "requirement": 2000}
+    ],
+    "strings": {
+        "saveSuccess": "神识已记录当前修为 (存档成功)",
+        "resetConfirm": "确定要斩断尘缘，重头来过吗？"
+    }
+}
